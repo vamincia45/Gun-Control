ee237a7666a8b16ba5cfe8fd493a40d44c878a1c
Amincia <53788203+vamincia45@users.noreply.github.com>
2019-08-16 16:01:53 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let canvas=document.querySelector('canvas');
+let canvas = document.querySelector('canvas');
 canvas.width=1000;
 canvas.height=500;
 
@@ -9,12 +9,33 @@ let cellSize=10;
 let ctx=canvas.getContext('2d');
 
 let data={
-  Australia:1000,
-  India:2700,
-  USA:500,
-  Brasil:2100,
-  China:3000
-
+  Ohio:5,
+  Texas:11,
+  California:19,
+  Virginia:3,
+  Illinois:16,
+  Maryland:3,
+  Tennessee:2,
+  Colorado:6,
+  Nevada:2,
+  Washington:8,
+  Louisiana:1,
+  Kansas:2,
+  Michigan:2,
+  Oregon:2,
+  Wisconsin:4,
+  NewYork:4,
+  Connecticut:3,
+  Minnesota:2,
+  Georgia:2,
+  Arizona:1,
+  Utah:1,
+  Massachusetts:1,
+  Arkansas:1,
+  SouthCarolina:2,
+  Iowa:1,
+  NorthCarolina:2,
+  Kentucky:2
 }
 
 const entries=Object.entries(data);
@@ -60,13 +81,34 @@ function drawAxis()
 
     for (var i=0;i<=10;i++)
     {
-        ctx.strokeText(pop, blocks(2), blocks(yPlot));
-        yPlot += 5;
-        pop += 500;
+        ctx.strokeText(pop,blocks(2),blocks(yPlot));
+        yPlot-=5;
+        pop+=500;
     }
+
     ctx.stroke();
+}
+
+function drawChart()
+{
+  ctx.beginPath();
+  ctx.strokeStyle="black";
+  ctx.moveTo(blocks(5),blocks(40));
+
+  var xPlot=10;
+
+  for(const [state,shooting] of entries)
+  {
+      var shootingInBlocks=populations/100;
+      ctx.strokeText(state,blocks(xPlot),blocks(40-shootingInBlocks-2));
+      ctx.lineTo(blocks(xPlot),blocks(40-shootingInBlocks));
+      ctx.arc(blocks(xPlot), blocks(40-shootingInBlocks),2,0,Math.PI*2,true);
+      xPlot+=5;
+  }
+  ctx.stroke();
 
 }
 
 drawGrids();
 drawAxis();
+drawChart();
